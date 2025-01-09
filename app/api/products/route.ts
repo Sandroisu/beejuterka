cb7de6f71ba24db793f9af60e29a22573c1b9b26
Alex Slatinin <sandroisu1411@gmail.com>
2025-01-09 22:06:28 +0300

    added route

---
+++ app/api/products/route.ts
@@ -0,0 +1,14 @@
+// app/api/products/route.ts
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+export async function GET() {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
+}
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  const newProduct = await prisma.product.create({ data });
+  return NextResponse.json(newProduct, { status: 201 });
+}
