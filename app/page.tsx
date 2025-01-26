56b71c574fd215d88455ef66c600c3b7026af463
Alex Slatinin <sandroisu1411@gmail.com>
2025-01-26 10:31:59 +0300

    added image to main screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 // app/page.tsxß
+import Image from 'next/image'
 
 export default async function HomePage() {
   return (
     <div className="pt-16 max-w-7xl mx-auto p-4">
-
+      <h1 className="text-2xl font-bold">Добро пожаловать в наш онлайн магазин бижутерии</h1>
+      <div>
+        <Image
+          src="/resources/images/main_screen_pic_1.jpg"
+          alt="Main screen pic"
+          width={1200}
+          height={800}
+          layout="intrinsic"
+        />
+      </div>
     </div>
   );
 }
