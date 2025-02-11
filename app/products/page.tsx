aa2c5875fc04105def00237eadea29583383e459
Alex Slatinin <sandroisu1411@gmail.com>
2025-02-11 06:59:17 +0300

    added route to product details

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,23 +1,25 @@
+import Link from "next/link";
 export default async function ProductsPage() {
-    const res = await fetch('http://localhost:3000/api/products', {
-      // Можно указать { cache: 'no-store' }, если нужно всегда свежие данные
-    });
-    if (!res.ok) {
-      throw new Error('Failed to fetch products');
-    }
-    const products = await res.json();
-  
-    return (
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Продукты</h1>
-        <ul>
-          {products.map((p: any) => (
-            <li key={p.id}>
-              {p.name} — {p.price} руб.
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  const res = await fetch('http://localhost:3000/api/products', {
+    // Можно указать { cache: 'no-store' }, если нужно всегда свежие данные
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
   }
-  
+  const products = await res.json();
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Продукты</h1>
+      <ul>
+        {products.map((p: any) => (
+          <li key={p.id}>
+            <Link href={`/products/${p.id}`}>
+              <p>{p.name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
