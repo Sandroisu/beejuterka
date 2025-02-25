27ee1d380a46a3d0f21be04263a3a24adb761b72
Alex Slatinin <sandroisu1411@gmail.com>
2025-02-25 07:20:44 +0300

    fixing cart route

--- app/cart/[cartId]/page.tsx
+++ app/cart/[cartId]/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 export default async function CartPage() {
-    const res = await fetch('http://localhost:3000/api/products', {
-        // Можно указать { cache: 'no-store' }, если нужно всегда свежие данные
+    const res = await fetch('http://localhost:3000/api/cart', {
+        cache: 'no-store'
     });
     if (!res.ok) {
         throw new Error('Failed to fetch products');
     }
-    const products = await res.json();
+    const cartItems = await res.json();
     return (
         <div>
             <h1>Your Cart</h1>
