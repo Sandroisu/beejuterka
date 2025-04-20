072df7eaeaebefa40a0b49e2140bf7d587a9858c
Alex Slatinin <sandroisu1411@gmail.com>
2025-04-20 20:55:43 +0300

    login form

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,7 @@
 
 import { Suspense } from 'react';
 import { Metadata } from 'next';
+import LoginForm from '../ui/login/login-form';
 
 export const metadata: Metadata = {
     title: 'Authentication',
@@ -11,7 +12,7 @@ export default function LoginPage() {
         <main className="flex items-center justify-center md:h-screen">
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <Suspense>
-
+                    <LoginForm />
                 </Suspense>
             </div>
         </main>
