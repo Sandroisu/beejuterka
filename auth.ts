5d3f921d9bf6c819b3964fe59ed91b150a090dec
Alex Slatinin <sandroisu1411@gmail.com>
2025-04-23 07:55:08 +0300

    added auth ts

---
+++ auth.ts
@@ -0,0 +1,38 @@
+
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: 'your-email@example.com' },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials, req): Promise<any> {
+                if (!credentials?.email || !credentials.password) {
+                    throw new Error('Email and password are required');
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email },
+                });
+
+                if (!user || user.password !== credentials.password) {
+                    throw new Error('Invalid email or password');
+                }
+
+                return user;
+            },
+        }),
+    ],
+    pages: { signIn: "/auth" },
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+});
