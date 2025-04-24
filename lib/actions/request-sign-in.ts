52ac3e6bcd9a15786773650f79b549a4d144a535
Alex Slatinin <sandroisu1411@gmail.com>
2025-04-24 07:17:04 +0300

    added action for auth

---
+++ lib/actions/request-sign-in.ts
@@ -0,0 +1,26 @@
+"use server"
+
+import { signIn } from "@/auth";
+import { redirect } from "@next/navigation";
+import { error } from "console";
+
+type SignInState = { error?: string };
+
+export async function requestSignIn(state: SignInState, formData: FormData) {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const callbackUrl = (formData.get("redirectTo") as string) || "/";
+
+    const result = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl,
+    });
+
+    if (result?.error) {
+        return { error: result.error }
+    }
+
+    redirect(callbackUrl);
+}
