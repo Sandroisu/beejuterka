dd7cc783acb9603c7128673f5b81fb0c9a68581c
Alex Slatinin <sandroisu1411@gmail.com>
2025-05-07 06:47:19 +0300

    fixed auth error

--- lib/actions/request-sign-in.ts
+++ lib/actions/request-sign-in.ts
@@ -3,6 +3,7 @@
 import { signIn } from "@/auth";
 import { redirect } from "next/navigation";
 import { error } from "console";
+import { AuthError } from "next-auth";
 
 export interface SignInState { error?: string };
 
@@ -11,16 +12,24 @@ export async function requestSignIn(state: SignInState, formData: FormData): Pro
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const callbackUrl = (formData.get("redirectTo") as string) || "/";
-    const result = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
-        callbackUrl,
-    });
-
-    if (result?.error) {
-        return { error: result.error }
+    try {
+        const result = await signIn("credentials", {
+            email,
+            password,
+            redirect: false,
+            callbackUrl,
+        });
+        if (result?.error) {
+            return { error: result.error }
+        }
+    } catch (e) {// error under useUnknownInCatchVariables 
+        if (typeof e === "string") {
+            return { error: e } // works, `e` narrowed to string
+        } else if (e instanceof Error) {
+            return { error: e.message }// works, `e` narrowed to Error
+        }
     }
+
 
 
     redirect(callbackUrl);
