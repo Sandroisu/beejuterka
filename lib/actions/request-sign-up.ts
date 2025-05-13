4ddb7ee81fac007b61b8fd2acffb8f3d18828e4f
Alex Slatinin <sandroisu1411@gmail.com>
2025-05-13 16:53:29 +0300

    added sign up function

---
+++ lib/actions/request-sign-up.ts
@@ -0,0 +1,41 @@
+"use server"
+
+import { hash } from "bcryptjs";
+import { prisma } from '@/lib/prisma';
+import { redirect } from "next/navigation";
+
+export interface SingUpState {
+    error?: string
+}
+
+export async function requestSignUp(state: SingUpState, formData: FormData): Promise<SingUpState> {
+    const email = formData.get("email") as string;
+    const name = (formData.get("name") as string)?.trim || null;
+    const password = formData.get("password") as string;
+    const confirm = formData.get("confirm") as string;
+    const redirectTo = (formData.get("redirectTo") as string) || "/auth";
+
+    if (!email || !password || password.length < 6) {
+        return { error: "Email и пароль необходимы (минимум 6 символов)" };
+    }
+
+    if (password !== confirm) {
+        return { error: "Пароли не совпадают" };
+    }
+
+    const existing = await prisma.user.findUnique({ where: { email } });
+    if (existing) {
+        return { error: "Пользователь с таким email уже существует" };
+    }
+
+    const hashed = await hash(password, 12);
+    await prisma.user.create({
+        data: {
+            email: email,
+            password: hashed,
+            name: name()
+        }
+    });
+
+    redirect(redirectTo)
+}
