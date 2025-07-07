1bbf1bef29336e9d3f44b5a6126488b89ffe14cd
Alex Slatinin <sandroisu1411@gmail.com>
2025-07-07 23:20:07 +0300

    changed from api to local

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { prisma } from '@/lib/prisma';
 export default async function ProductsPage() {
   const res = await fetch('http://localhost:3000/api/products', {
     // Можно указать { cache: 'no-store' }, если нужно всегда свежие данные
@@ -7,12 +8,12 @@ export default async function ProductsPage() {
     throw new Error('Failed to fetch products');
   }
   const products = await res.json();
-
+  const productsFromPrisma = await prisma.product.findMany()
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Продукты</h1>
       <ul>
-        {products.map((p: any) => (
+        {productsFromPrisma.map((p: any) => (
           <li key={p.id}>
             <Link href={`/product/${p.id}`}>
               <p>{p.name}</p>
