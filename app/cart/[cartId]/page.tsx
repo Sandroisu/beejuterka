fe0194b2b51ff1cf2038a49df0aadd0565fb1658
Alex Slatinin <sandroisu1411@gmail.com>
2025-07-23 22:09:19 +0300

    fiin cart page

--- app/cart/[cartId]/page.tsx
+++ app/cart/[cartId]/page.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 
 export default async function CartPage({ params }: { params: { cartId: string } }) {
     const { cartId } = params;
-    const res = await fetch('http://localhost:3000/api/cart', {
-        cache: 'no-store'
-    });
-    if (!res.ok) {
-        throw new Error('Failed to fetch cart data');
-    }
-    const cartItems = await res.json();
+    const res = await prisma.cart.findUnique({
+        where: {
+            id: cartId
+        }
+    })
+
+
     return (
         <div>
             <h1>Your Cart</h1>
-            <p>Your cart is empty.</p>
+            {res === null ? : <p>Your cart is empty.</p>}
             <Link href="/products">Continue Shopping</Link>
         </div>
     );
