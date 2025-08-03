37373b22dc9503120cc3ce7490290286ca796736
Alex Slatinin <sandroisu1411@gmail.com>
2025-08-03 07:41:01 +0300

    added product relation to cart item

--- app/cart/[cartId]/page.tsx
+++ app/cart/[cartId]/page.tsx
@@ -4,17 +4,25 @@ import Link from "next/link";
 
 export default async function CartPage({ params }: { params: { cartId: string } }) {
     const { cartId } = params;
-    const res = await prisma.cart.findUnique({
+    const res = await prisma.cartItem.findMany({
         where: {
-            id: cartId
-        }
+            cartId: cartId
+        },
     })
 
 
     return (
         <div>
             <h1>Your Cart</h1>
-            {res === null ? : <p>Your cart is empty.</p>}
+            {res.length === 0 ? <p>Your cart is empty.</p> : res.map(
+                (item: any) => (
+                    <div key={item.id}>
+                        <h2>{item.product.name}</h2>
+                        <p>Price: {item.product.price}</p>
+                        <p>Quantity: {item.quantity}</p>
+                    </div>
+                )
+            )}
             <Link href="/products">Continue Shopping</Link>
         </div>
     );
